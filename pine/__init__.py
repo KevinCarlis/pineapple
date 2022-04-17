809ac586107e51980d565edf8c3c41960308f787
Kevin Carlis <kevcarlis@gmail.com>
2022-04-17 13:24:00 -0700

    Rewrote ABC

--- pine/__init__.py
+++ pine/__init__.py
@@ -1,6 +1,6 @@
 """ Pineapple is a version of Chinese poker.
 """
-PINEAPPLE_PATH = '~/'
-import sys
-if not PINEAPPLE_PATH in sys.path:
-        sys.path.append(PINEAPPLE_PATH)
+#PINEAPPLE_PATH = '~/'
+#import sys
+#if not PINEAPPLE_PATH in sys.path:
+#        sys.path.append(PINEAPPLE_PATH)
